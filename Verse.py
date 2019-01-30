45f32c4b9b782c71d75d7e796b48507adb10d0d5
Brady <goldsworthybrady@gmail.com>
2019-01-30 14:50:01 -0600

    Done for now

--- Verse.py
+++ Verse.py
@@ -7,7 +7,7 @@ today = date.today()
 
 print("Todays date: ", today)
 
-TOTAL_YEARS = 50 #Amount of future years to consider
+TOTAL_YEARS = 100 #Amount of future years to consider
 year_counter = 0
 
 weekend_counter = 0 #Counter of how many weekend days chosen date will have in next YEARS amount of years
@@ -22,8 +22,7 @@ print("Date1:", date1)
 print("Date2:", date2)
 
 while year_counter < TOTAL_YEARS :
-	date1 = date1 + relativedelta(years=+1)
-	date2 = date2 + relativedelta(years=+1)
+	year_counter += 1
 
 	if date1.weekday() > 3 :
 		date1weekenddays += 1
@@ -31,8 +30,12 @@ while year_counter < TOTAL_YEARS :
 	if date2.weekday() > 3 :
 		date2weekenddays += 1
 
-	year_counter += 1
+	date1 = date1 + relativedelta(years=+1)
+	date2 = date2 + relativedelta(years=+1)
 
+	if year_counter % 20 == 0:
+		print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		print("December 5th has", date1weekenddays, "weekend days in the next", year_counter, "years.")
+		print("December 12th has", date2weekenddays, "weekend days in the next", year_counter, "years.")
 
-print("Date 1 has", date1weekenddays, "weekend days in the next", TOTAL_YEARS, "years.")
-print("Date 2 has", date2weekenddays, "weekend days in the next", TOTAL_YEARS, "years.")
+	
